3a64a09e5773690b7a9ace1e1b137e2a026f058d
Gustavo Paulo <gustavo.p07@aluno.ifsc.edu.br>
2023-10-04 14:36:12 -0300

    update fstring

--- basico/fstring.py
+++ basico/fstring.py
@@ -1,3 +1,9 @@
 x = 'Uma variável qualquer'
 y = f'Essa é uma string com uma variável: {x}'
 print(y)
+
+
+print(f'{x: >50}') # Alinhando a direita
+print(f'{x: <50}') # Alinhando a esquerda
+print(f'{x: ^50}') # Centralizando
+print(f'{x:*^50}') # Centralizando e preenchendo com asteriscos
