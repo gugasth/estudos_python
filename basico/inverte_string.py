4ad7cc2851d9a49afd9b2e391b61ebf225b7d252
Gustavo Paulo <gustavo.p07@aluno.ifsc.edu.br>
2023-10-04 14:43:18 -0300

    Inverte String

---
+++ basico/inverte_string.py
@@ -0,0 +1,7 @@
+frase = 'Uma frase qualquer'
+print(f'Antes de inverter: {frase}')
+print(f'Depois de inverter: {frase[::-1]}') # Invertendo a string
+
+print()
+frase_invertida = frase[::-1]
+print('Frase invertida é:', frase_invertida)
