8a37fec78b54c3aeac9479b35217c7b0c5ec937c
Gustavo Paulo <gustavo.p07@aluno.ifsc.edu.br>
2023-10-04 13:59:12 -0300

    float

---
+++ basico/float.py
@@ -0,0 +1,7 @@
+# Arredondando valores de ponto flutuante
+
+altura = 1.8
+pi = 3.141592653589793
+
+print(f'Altura: {altura:.2f}') # Arredondando 2 casas após a vírgula
+print(f'Altura: {pi:.4f}') # Arredondando 4 casas após a vírgula
