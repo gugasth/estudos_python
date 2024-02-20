820ec5afe6b79971737db373d2b90eeaf0f9c70e
Gustavo Paulo <gustavo.p07@aluno.ifsc.edu.br>
2024-02-19 21:10:02 -0300

    :sparkles: feat: setter + property

--- oop/@property.py
+++ oop/@property.py
@@ -1,51 +1,36 @@
-# @property - um getter no modo Pythônico
-# getter - um método para obter um atributo
-# cor -> get_cor()
-# modo pythônico - modo do Python de fazer coisas
-# @property é uma propriedade do objeto, ela
-# é um método que se comporta como um
-# atributo 🤯 🤯 🤯
-# Geralmente é usada nas seguintes situações:
-# - como getter
+# @property + @setter - getter e setter no modo Pythônico
 # - p/ evitar quebrar código cliente
 # - p/ habilitar setter
 # - p/ executar ações ao obter um atributo
-# Código cliente - é o código que usa seu código
+# Atributos que começar com um ou dois underlines
+# não devem ser usados fora da classe.
 class Caneta:
     def __init__(self, cor):
-        self.cor_tinta = cor
+        # private protected
+        self.cor = cor
+        self._cor_tampa = None
 
     @property
     def cor(self):
-        print('PROPERTY')
-        return self.cor_tinta
+        print('ESTOU NO GETTER')
+        return self._cor
+
+    @cor.setter
+    def cor(self, valor):
+        print('ESTOU NO SETTER')
+        self._cor = valor
 
     @property
     def cor_tampa(self):
-        return 123456
+        return self._cor_tampa
 
-###########################
+    @cor_tampa.setter
+    def cor_tampa(self, valor):
+        self._cor_tampa = valor
 
 
 caneta = Caneta('Azul')
-print(caneta.cor)
-print(caneta.cor)
+caneta.cor = 'Rosa'
+caneta.cor_tampa = 'Azul'
 print(caneta.cor)
 print(caneta.cor_tampa)
-
-#####################################
-
-# class Caneta:
-#     def __init__(self, cor):
-#         self.cor_tinta = cor
-
-#     def get_cor(self):
-#         print('GET COR')
-#         return self.cor_tinta
-
-# ###########################
-
-# caneta = Caneta('Azul')
-# print(caneta.get_cor())
-# print(caneta.get_cor())
-# print(caneta.get_cor())
